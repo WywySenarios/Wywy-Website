4313009187910707f594ddd5cbfdee95b3008d05
WywySenarios <wywyericzhu@gmail.com>
2024-11-15 15:46:08 -0500

    personal & ShadCN
    Started on the personal section of the website. Imported ShadCN & adjusted tailwind for ShadCN.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
 
   adapter: node({
     mode: 'middleware'
-  })
+  }),
+
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })]
 });
