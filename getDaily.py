02d2ca097234b6a225465a606f8c19b7a2bee2db
WywySenarios <wywyericzhu@gmail.com>
2024-11-18 19:01:21 -0500

    Google Sheets API
    FINISHED THE PYTHON SCRIPT!!! It now gathers all cells, strips notes that I don't want to show, and outputs it in data/daily.json !!
    
    Maybe later I should add a path option to customize where daily.json is put. That might be fun :D

--- getDaily.py
+++ getDaily.py
@@ -10,18 +10,18 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = "1reqTRELv3O9aNCOr1_wG2pbsL14POf8uvKY2uPpwh-U"
-DATA_SHEET_NAME = "Daily Data"
-
 # Open and read the config filepath file
 with open('config.json', 'r') as file:
     cfgFilepaths = json.load(file)
 
+# The ID and range of the spreadsheet.
+SPREADSHEET_ID = cfgFilepaths.get("personalSheet").get("id")
+DATA_SHEET_NAME = "Daily Data"
+
 """
-Returns values of the given range
+Returns values of the given range, and notes, too, if you wanted them :)
 """
-def getRange(rangeName):
+def getRange(rangeName, notes):
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
@@ -45,49 +45,70 @@ def getRange(rangeName):
     service = build("sheets", "v4", credentials=creds)
 
     # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SPREADSHEET_ID, range=rangeName)
-        .execute()
-    )
-    values = result.get("values", [])
-
-    if not values:
-      return None # no data found :(
+    data = None
+    if (notes):
+      # get ALL THE INFO ABOUT THE CELLS :)))))
+      sheet = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID, ranges=[rangeName], includeGridData=True).execute()
+      
+      data = []
+      for sheet in sheet.get("sheets", []):
+        for row_data in sheet["data"][0].get('rowData', []):
+          row = []
+          for cell in row_data.get('values', []):
+            note = cell.get('note', '') # fetch cell note
+            value = cell.get('formattedValue', '')
+            # value = cell.get('effectiveValue', {}).get('stringValue', '')
+            row.append({"note": note, "value": value})
+          data.append(row)
+    else:
+      sheet = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
+      data = sheet.get("values", [])
     
-    return values
+    return data
   except HttpError as err:
+    print("Yikes")
     return None # error encountered :(
 
+
+"""
+"personalSheet": {
+  "id": "1reqTRELv3O9aNCOr1_wG2pbsL14POf8uvKY2uPpwh-U",
+  "ranges": {
+    "Qualitative": {
+      "dataSheetName": "Daily Data",
+      "range": "B3:J",
+      "notes": ["B3:B", "E3:J"] 
+    },
+  "Quantitative": {
+      "dataSheetName": "Daily Data",
+      "range": "M3:U",
+      "notes": []
+    }
+  }
+}
+"""
 def main():
-  dailyQualitative = getRange(DATA_SHEET_NAME + "!A2:J")
-  dailyQuantitative = getRange(DATA_SHEET_NAME + "!M2:U")
+  allData = json.loads("{}")
+  a = 0
+  ranges = cfgFilepaths.get("personalSheet").get("ranges")
+  for itemName in ranges:
+    item = ranges.get(itemName)
+    currentData = json.loads("{}")
+    
+    
+    currentData["headers"] = getRange(DATA_SHEET_NAME + "!" + item["headers"]["range"], item["headers"]["notes"])
+    currentData["content"] = getRange(DATA_SHEET_NAME + "!" + item["content"]["range"], item["content"]["notes"])
+    
+    
+    # strip notes within a range specified by the config
+    for i in item["content"]["strip_notes"]:
+      for a in currentData["content"]:
+        a[i]["note"] = ""
+    allData[itemName] = currentData
 
-  # convert string to float values when applicable
-  for a in range(len(dailyQualitative)):
-    for b in range(len(dailyQualitative[a])):
-      try:
-        dailyQualitative[a][b] = float(dailyQualitative[a][b])
-      except:
-        pass
-
-  for a in range(len(dailyQuantitative)):
-    for b in range(len(dailyQuantitative[a])):
-      try:
-        dailyQuantitative[a][b] = float(dailyQuantitative[a][b])
-      except:
-        pass
-
-  try:
-    open("data/daily.json", "x")
-  except:
-    pass
-
+  # write to file :)))
   dailyFile = open("data/daily.json", "w")
-
-  dailyFile.write("{" + "\"Qualitative\":{\"headers\":" + str(dailyQualitative.pop(0)).replace("\'", "\"") + ",\"values\":" + str(dailyQualitative).replace("\'", "\"") + "},\"Quantitative\":{\"headers\":" + str(dailyQuantitative.pop(0)).replace("\'", "\"") + ",\"values\":" + str(dailyQuantitative).replace("\'", "\"") + "}}")
-
+  dailyFile.write(json.dumps(allData))
   dailyFile.close()
 
 
