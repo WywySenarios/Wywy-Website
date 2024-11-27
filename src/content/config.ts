c26cb68677e9efdbf5dc06f0e13fd1bc1fc6be54
WywySenarios <wywyericzhu@gmail.com>
2024-11-26 20:20:58 -0500

    Blog
    There is no option to disable HTML rendering for blog posts. I think it's hard to implement, and it doesn't seem like a big detriment to the type of content that can be posted.

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,5 +13,13 @@ export const collections = {
         "description": z.string(),
         "date": z.string(),
         "imagePath": z.string().optional(),
+    })}),
+    "blog": defineCollection({ type: 'content', schema: z.object({
+        "url": z.string(),
+        "title": z.string(),
+        "description": z.string(),
+        "publicationDate": z.string(),
+        "dateLastUpdated": z.string(),
+        "category": z.array(z.string()),
     })})
 };
