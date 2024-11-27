268a41fd59a4082df91e0bc4ce535cd39eb64808
WywySenarios <wywyericzhu@gmail.com>
2024-11-27 11:49:57 -0500

    Privacy Policy & Remove useless Import
    Removed a useless import from blog pages.
    
    Revamped the privacy policy pages to take markdown files and create a privacy policy page. CSS may appear to be inconsistent with the rest of the website (is:global was used in privacyPolicy.astro)

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,24 +2,39 @@ import { z, defineCollection } from 'astro:content';
 
 
 export const collections = {
-    "awards": defineCollection({ type: 'content', schema: z.object({
-        "title": z.string(),
-        "description": z.string(),
-        "date": z.string(),
-        "imagePath": z.string().optional(),
-    }) }),
-    "directives": defineCollection({ type: 'content', schema: z.object({
-        "title": z.string(),
-        "description": z.string(),
-        "date": z.string(),
-        "imagePath": z.string().optional(),
-    })}),
-    "blog": defineCollection({ type: 'content', schema: z.object({
-        "url": z.string(),
-        "title": z.string(),
-        "description": z.string(),
-        "publicationDate": z.string(),
-        "dateLastUpdated": z.string(),
-        "category": z.array(z.string()),
-    })})
+    "awards": defineCollection({
+        type: 'content', schema: z.object({
+            "title": z.string(),
+            "description": z.string(),
+            "date": z.string(),
+            "imagePath": z.string().optional(),
+        })
+    }),
+    "directives": defineCollection({
+        type: 'content', schema: z.object({
+            "title": z.string(),
+            "description": z.string(),
+            "date": z.string(),
+            "imagePath": z.string().optional(),
+        })
+    }),
+    "blog": defineCollection({
+        type: 'content', schema: z.object({
+            "url": z.string(),
+            "title": z.string(),
+            "description": z.string(),
+            "publicationDate": z.string(),
+            "dateLastUpdated": z.string(),
+            "category": z.array(z.string()),
+        })
+    }),
+    "privacyPolicies": defineCollection({
+        type: 'content', schema: z.object({
+            "title": z.string(),
+            "dateLastUpdated": z.coerce.date(),
+            "summary": z.string(),
+            "datapointsReleased": z.array(z.string()),
+            "datapointsKeptPrivate": z.array(z.string()),
+        })
+    })
 };
---
+++ src/content/privacyPolicies/examplePolicy.md
@@ -0,0 +1,7 @@
+---
+"title": "My Policy"
+"dateLastUpdated": 11/27/2024
+"summary": "You should consider your RPS Runtime data as open source, which means that anyone can access it."
+"datapointsReleased": ["wywyness"]
+"datapointsKeptPrivate": ["height", "name"]
+---
