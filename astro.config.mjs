0a2e4ab136d6ecdee7836d1b477a7164e410aa50
WywySenarios <wywyericzhu@gmail.com>
2024-12-20 14:44:10 -0500

    Default Website Title in Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,29 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import { networkInterfaces } from 'os';
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    // @ts-ignore
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.ericzhu.me",
   output: 'server',
 
   adapter: node({
@@ -14,4 +34,7 @@ export default defineConfig({
   }),
 
   integrations: [tailwind(), react()],
+  server: ({command}) => (
+    { port: 5322, host: results["Wi-Fi"][0] }
+  )
 });
