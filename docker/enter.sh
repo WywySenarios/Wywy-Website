4fa8090fc01bef8bffed8d348e0f0c13907af56b
WywySenarios <wywyericzhu@gmail.com>
2025-10-12 11:58:12 -0400

    Dockerized Astro Dev Server

--- docker/enter.sh
+++ docker/enter.sh
@@ -1,4 +1,5 @@
-TARGET="docker-sql-receptionist"
+#!/bin/bash
+TARGET="docker-astro-app"
+VOLUMES="../apps/astro-app:/apps/astro-app"
 
-# Enter the image
-sudo docker run -it --rm $TARGET /bin/sh
+sudo docker run -it --rm $VOLUMES -p 4321:4321 -p 2523:2523 $TARGET /bin/sh
