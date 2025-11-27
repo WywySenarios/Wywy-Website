cdcb67dd571502ef0905fdee252822e128513480
WywySenarios <wywyericzhu@gmail.com>
2025-11-27 11:02:58 -0500

    Astro Cloudflare Adapter

--- apps/astro-app/astro.config.mjs
+++ apps/astro-app/astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import yaml from '@rollup/plugin-yaml';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 
   vite: {
-    plugins: [tailwindcss(), yaml()]
+    plugins: [tailwindcss(), yaml()],
+    // build: {
+    //   minify: false,
+    // },
   },
 
   integrations: [react()]
