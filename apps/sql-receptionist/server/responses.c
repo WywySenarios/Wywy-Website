96ddebb20142eb518e50d130ad9c51d0b21574d1
WywySenarios <wywyericzhu@gmail.com>
2025-12-07 22:40:48 -0500

    HTTP Response Building Library General Structure
    No comments or constants are added yet.

---
+++ apps/sql-receptionist/server/responses.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "config.h"
+
+static struct config *global_config = NULL;
+
+void init(config cfg) {
+    global_config = cfg;
+
+    // @TODO check validity
+}
+
+void build_response_generic(int status_code, char **response, size_t *response_len, char *body) {
+    const char *status_code_name;
+    switch (status_code) {
+        default:
+            status_code_name = "";
+    }
+
+    response_len = strlen("HTTP/1.1 xxx \r\n"
+                           "Content-Type: text/plain\r\n"
+                           "Access-Control-Allow-Origin: \r\n"
+                           "Access-Control-Allow-Credentials: true\r\n"
+                           "Connection: close\r\n"
+                           "\r\n") + strlen(status_code_name) + strlen(global_config->reference_urls.main) + strlen(body);
+    response = malloc(response_len + 1);
+    snprintf(response, repsonse_len + 1, "HTTP/1.1 %d %s\r\n"
+                           "Content-Type: text/plain\r\n"
+                           "Access-Control-Allow-Origin: %s\r\n"
+                           "Access-Control-Allow-Credentials: true\r\n"
+                           "Connection: close\r\n"
+                           "\r\n%s", status_code, status_code_name, global_config->reference_urls.main, body);
+}
+
+void build_response(int status_code, char **response, size_t *response_len, const char *pattern, size_t text_size, ...) {
+    va_list arg;
+    char *body = malloc(text_size + 1);
+    va_start(arg, pattern);
+    vsnprintf(body, text_size, pattern, arg);
+    va_end(arg);
+
+    build_response_generic(repsonse, response_len, body);
+    
+    free(body);
+}
+
+void build_response_default(int status_code, char **response, size_t *response_len) {}
