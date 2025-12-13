98423a92112954a2a9815bff80dd37db7268da7f
WywySenarios <wywyericzhu@gmail.com>
2025-12-12 20:57:11 -0500

    #include <stdbool.h>

--- apps/sql-receptionist/config.h
+++ apps/sql-receptionist/config.h
@@ -1,46 +1,42 @@
-struct reference_urls
-{
-    const char *main;
-    const char *db;
+#include <stdbool.h>
+
+struct reference_urls {
+  const char *main;
+  const char *db;
 };
 
-struct postgres_config
-{
-    const char *host; // @todo validation
+struct postgres_config {
+  const char *host; // @todo validation
 };
 
-struct data_column
-{
-    const char *name;                  // @todo validation
-    const char *datatype;              // @todo validation
-    const char *invalid_input_message; // optional
-    bool comments;                     // optional, ?useless here?
-    const char *entrytype;             // useless here
+struct data_column {
+  const char *name;                  // @todo validation
+  const char *datatype;              // @todo validation
+  const char *invalid_input_message; // optional
+  bool comments;                     // optional, ?useless here?
+  const char *entrytype;             // useless here
 };
 
-struct table
-{
-    char *table_name;      // @todo validation
-    bool read;             // @todo validation
-    bool write;            // @todo validation
-    const char *entrytype; // @todo validation
-    struct data_column *schema;
-    unsigned int schema_count;
+struct table {
+  char *table_name;      // @todo validation
+  bool read;             // @todo validation
+  bool write;            // @todo validation
+  const char *entrytype; // @todo validation
+  struct data_column *schema;
+  unsigned int schema_count;
 };
 
-struct db
-{
-    char *db_name; // @todo validation
-    struct table *tables;
-    unsigned int tables_count;
+struct db {
+  char *db_name; // @todo validation
+  struct table *tables;
+  unsigned int tables_count;
 };
 
-struct config
-{
-    struct reference_urls reference_urls;
-    struct postgres_config postgres;
-    struct db *dbs;
-    unsigned int dbs_count;
+struct config {
+  struct reference_urls reference_urls;
+  struct postgres_config postgres;
+  struct db *dbs;
+  unsigned int dbs_count;
 };
 
 extern void load_config(struct config **cfg);
