c44aecd2dbb8dbe06f50960b6b2855e94cf75536
WywySenarios <wywyericzhu@gmail.com>
2025-12-15 21:23:05 -0500

    Add extern statements for utils/parse_regex

--- apps/sql-receptionist/utils/parse_regex.h
+++ apps/sql-receptionist/utils/parse_regex.h
@@ -7,3 +7,14 @@ struct regex_iterator {
   char *target;
   char *cur;
 };
+
+extern struct regex_iterator *
+create_regex_iterator(char *pattern, int num_matches, int cflags);
+extern void regex_iterator_load_target(struct regex_iterator *iter,
+                                       char *new_target);
+extern void regex_iterator_replace_target(struct regex_iterator *iter,
+                                          char *new_target);
+extern int regex_iterator_peek(struct regex_iterator *iter, int eflags);
+int regex_iterator_match_next(struct regex_iterator *iter, int eflags);
+char *regex_iterator_get_match(struct regex_iterator *iter, int match_num);
+void free_regex_iterator(struct regex_iterator *iter);
