d4fd59b0205d580eac410ae029e43f173e387fac
WywySenarios <wywyericzhu@gmail.com>
2025-12-20 22:13:42 -0500

    Inject secret & env file for enter.sh

--- docker/enter.sh
+++ docker/enter.sh
@@ -11,7 +11,12 @@ case "$1" in
     sudo docker run -it --rm -p 4321:4321 -v "../apps/astro-app:/apps/astro-app" docker-astro-app /bin/sh
     ;;
   sqlr)
-    sudo docker run -it --rm -p 2523:2523 docker-sql-receptionist /bin/sh
+    sudo docker run -it --rm \
+    -p 2523:2523 \
+    --env-file .env \
+    --mount type=bind,source="$(pwd)/../secrets/admin.txt",target=/run/secrets/admin,readonly \
+    docker-sql-receptionist \
+    /bin/sh
     ;;
   pgres)
     sudo docker run -it --rm -p 5432:5432 -v "postgres-db:/var/lib/postgresql" docker-postgres /bin/sh
