32feeec26f1afd60aaa7f713ade6c8bdf2875e6b
WywySenarios <wywyericzhu@gmail.com>
2026-01-01 17:29:51 -0500

    Add JSDoc comments

--- apps/astro-app/src/components/data/data-entry-form.tsx
+++ apps/astro-app/src/components/data/data-entry-form.tsx
@@ -7,8 +7,12 @@ import { TimerForm } from "@/components/data/data-entry/timer"
 
 /**
  * Selects the correct form element to use. Expects there to be a valid Toast element inside the page.
+ * @param entrytype The entrytype of the form.
+ * @param fieldsToEnter
+ * @param databaseName The name of the database that this form gathers data for.
+ * @param tableName The name of the table that this form gathers data for.
+ * @param dbURL The URL that the form will post to on submit.
  */
-
 export default function DataEntryForm({
     entrytype,
     fieldsToEnter,
