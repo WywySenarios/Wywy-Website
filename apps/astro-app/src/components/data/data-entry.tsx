f09e4b44f31b06dffc5de0e0475944633add883b
WywySenarios <wywyericzhu@gmail.com>
2026-01-03 22:10:18 -0500

    Headers/template for tagging & descriptors

--- apps/astro-app/src/components/data/data-entry.tsx
+++ apps/astro-app/src/components/data/data-entry.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from "astro/jsx-runtime";
 import { FormElement } from "@/components/data/input-elements";
-import type { DataColumn } from "@/env";
+import type { DataColumn, TableInfo } from "@/env";
 import type { UseFormReturn } from "react-hook-form";
 
 export function Columns({
@@ -20,5 +20,45 @@ export function Columns({
             )
             }
         </div>
+    )
+}
+
+/**
+ * The tagging related form component.
+ * @param databaseName The name of the database that this form gathers data for.
+ * @param tableName The name of the table that this form gathers data for.
+ * @param dbURL The URL that the form will post to on submit.
+ * @returns 
+ */
+export function Tags({
+
+}: {
+    databaseName: string,
+    tableName: string,
+    dbURL: string
+}): JSX.Element {
+    return (
+        <></>
+    )
+}
+
+/**
+ * The desciptor related form component.
+ * @param databaseName The name of the database that this form gathers data for.
+ * @param tableInfo The schema for the respective table.
+ * @param dbURL The URL that the form will post to on submit.
+ * @returns 
+ */
+export function Descriptors({
+    databaseName,
+    tableInfo,
+    dbURL
+}: {
+    databaseName: string,
+    tableInfo: TableInfo
+    dbURL: string
+}): JSX.Element {
+    return (
+        <></>
     )
 }
