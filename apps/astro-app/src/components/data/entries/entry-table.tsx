e7d83ccef50e89e32d9ab6b1e146a67b9f59cfc6
WywySenarios <wywyericzhu@gmail.com>
2026-01-13 22:43:17 -0500

    EntryTable.tsx basic structure

---
+++ apps/astro-app/src/components/data/entries/entry-table.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import type { DescriptorInfo, TableInfo } from "@/env";
+import type { JSX } from "astro/jsx-runtime";
+
+/**
+ * Renders an EntryTable to view and modify entries on specific tables.
+ * @param schema The schema of the table.
+ * @param databaseName The name of the target database
+ * @param parentTableName The name of the parent table, or the target table if the table has no parent.
+ * @param tableName The name of the target table.
+ * @param type The type of table to render. Is either undefined (generic) or a tagging table type.
+ * @returns an EntryTable component.
+ */
+function EntryTable({
+  schema,
+  databaseName,
+  parentTableName,
+  tableName,
+  type,
+}: {
+  schema?: TableInfo | DescriptorInfo | undefined;
+  databaseName: string;
+  parentTableName: string;
+  tableName: string;
+  type?: undefined | "tags" | "tag_names" | "tag_aliases" | "tag_groups";
+}): JSX.Element {
+  switch (type) {
+    case undefined:
+      if (schema == undefined) return <NoTable />;
+
+      return (
+        <GenericEntryTable
+          schema={schema}
+          databaseName={databaseName}
+          parentTableName={parentTableName}
+          tableName={tableName}
+        />
+      );
+    case "tags":
+      return (
+        <TagsTable
+          databaseName={databaseName}
+          parentTableName={parentTableName}
+          tableName={tableName}
+        />
+      );
+    case "tag_names":
+      return (
+        <TagNamesTable
+          databaseName={databaseName}
+          parentTableName={parentTableName}
+          tableName={tableName}
+        />
+      );
+    case "tag_aliases":
+      return (
+        <TagAliasesTable
+          databaseName={databaseName}
+          parentTableName={parentTableName}
+          tableName={tableName}
+        />
+      );
+    case "tag_groups":
+      return (
+        <TagGroupsTable
+          databaseName={databaseName}
+          parentTableName={parentTableName}
+          tableName={tableName}
+        />
+      );
+  }
+}
+
+/**
+ * Component to be rendered when something is wrong. @TODO pretty up this component
+ */
+function NoTable(): JSX.Element {
+  <p>Something went wrong.</p>;
+}
+
+/**
+ * A generic entry editor table.
+ * @param schema The table schema.
+ * @param databaseName The name of the database that contains the target table.
+ * @param parentTableName The name of the parent table (or the target table if there is no parent).
+ * @param tableName The name of the target table.
+ * @returns
+ */
+function GenericEntryTable({
+  schema,
+  databaseName,
+  parentTableName,
+  tableName,
+}: {
+  schema: TableInfo | DescriptorInfo;
+  databaseName: string;
+  parentTableName: string;
+  tableName: string;
+}): JSX.Element {
+  return <></>;
+}
+
+interface TaggingEntryTableProps {
+  databaseName: string;
+  parentTableName: string;
+  tableName: string;
+}
+
+/**
+ * A tags editor table. May add or remove tags links from tags to generic entries.
+ * @param databaseName The name of the database that contains the target table.
+ * @param parentTableName The name of the parent table (or the target table if there is no parent).
+ * @param tableName The name of the target table.
+ * @returns
+ */
+function TagsTable({
+  databaseName,
+  parentTableName,
+  tableName,
+}: TaggingEntryTableProps): JSX.Element {
+  return <></>;
+}
+/**
+ * A tag name editor table. May create new tags but cannot remove old tags.
+ * @param databaseName The name of the database that contains the target table.
+ * @param parentTableName The name of the parent table (or the target table if there is no parent).
+ * @param tableName The name of the target table.
+ * @returns
+ */
+function TagNamesTable({
+  databaseName,
+  parentTableName,
+  tableName,
+}: TaggingEntryTableProps): JSX.Element {
+  return <></>;
+}
+/**
+ * A tags aliases table. May create or remove tag aliases. The user is not prevented from removing all aliases from a given tag, nor are they barred from removing the respective tag's direct name.
+ * @param databaseName The name of the database that contains the target table.
+ * @param parentTableName The name of the parent table (or the target table if there is no parent).
+ * @param tableName The name of the target table.
+ * @returns
+ */
+function TagAliasesTable({
+  databaseName,
+  parentTableName,
+  tableName,
+}: TaggingEntryTableProps): JSX.Element {
+  return <></>;
+}
+/**
+ * A tag group editor table. May create or remove links from tags to tag groups.
+ * @param databaseName The name of the database that contains the target table.
+ * @param parentTableName The name of the parent table (or the target table if there is no parent).
+ * @param tableName The name of the target table.
+ * @returns
+ */
+function TagGroupsTable({
+  databaseName,
+  parentTableName,
+  tableName,
+}: TaggingEntryTableProps): JSX.Element {
+  return <></>;
+}
+
+export default EntryTable;
