dc3d63057caebe24ba86fc290f93dd6a63d79fde
WywySenarios <wywyericzhu@gmail.com>
2026-01-25 20:50:58 -0500

    Refractor construct_validate_query (better memory management)

--- apps/sql-receptionist/src/postgres/insert.c
+++ apps/sql-receptionist/src/postgres/insert.c
@@ -29,28 +29,23 @@ int construct_validate_query(json_t *entry, struct data_column *schema,
   const char *key = NULL;
   const json_t *value = NULL;
   char *value_string = NULL;
-  char *snake_case_key = NULL;
+  size_t column_name_size = 0;
+  char *column_name = NULL;
 
-  // the status of the query.
-  // 0 if the query is invalid,
-  // 1 if the query is valid,
-  // -1 on unexpected (e.g. malloc failure)
+  size_t column_names_size = 1; // null-terminator
+  char *column_names = NULL;
+  size_t values_size = 1; // null-terminator
+  char *values = NULL;
+
+  // The status (return value) of the query validation and construction.
   int status = 1;
 
-  int values_len = 0;
-  int column_names_len = 0;
-  int separator_len = 0;
-
+  // check for conformance
   json_object_foreach(entry, key, value) {
     bool valid = false;
     value_string = json_to_string(value);
 
-    if (!value_string) {
-      return -1;
-    }
-
-    size_t target_column_len = strlen(key) + 1;
-    char *target_column = NULL;
+    column_name_size = strlen(key) + 1;
 
     // check for the case in which the JSON key relates to comments instead
     // of regular columns
@@ -60,22 +55,27 @@ int construct_validate_query(json_t *entry, struct data_column *schema,
     case 0:
       break;
     case 1:
-      target_column_len -= strlen("_comments");
+      column_name_size -= strlen("_comments");
       break;
     default:
       status = -1;
       goto construct_validate_query_end;
     }
-    target_column = malloc(target_column_len);
-    if (!target_column) {
+    column_name = malloc(column_name_size);
+
+    // check for malloc failures
+    if (!value_string || !column_name) {
       status = -1;
       goto construct_validate_query_end;
     }
-    memcpy(target_column, key, target_column_len - 1);
-    target_column[target_column_len - 1] = '\0';
+    memcpy(column_name, key, column_name_size - 1);
+    column_name[column_name_size - 1] = '\0';
 
-    if (strcmp(target_column, "id") == 0) {
+    if (strcmp(column_name, "id") == 0) {
       // skip id column (postgres autoincrement should handle it)
+      free(value_string);
+      free(column_name);
+      continue;
 
       // @TODO make sure postgres doesn't tweak out over incorrect next keys
       switch (regex_check("^[0-9]+$", 0, REG_EXTENDED, 0, value_string)) {
@@ -86,12 +86,10 @@ int construct_validate_query(json_t *entry, struct data_column *schema,
         valid = true;
         break;
       default:
-        // @todo cleaner looking way of exiting
-        free(target_column);
-        return -1;
-        break;
+        status = -1;
+        goto construct_validate_query_end;
       }
-    } else if (strcmp(target_column, "primary_tag") == 0) {
+    } else if (strcmp(column_name, "primary_tag") == 0) {
       // it's hard to validate FOREIGN KEY so we'll let Postgres take care of
       // this.
       switch (regex_check("^[0-9]+$", 0, REG_EXTENDED, 0, value_string)) {
@@ -102,16 +100,14 @@ int construct_validate_query(json_t *entry, struct data_column *schema,
         valid = true;
         break;
       default:
-        // @todo cleaner looking way of exiting
-        free(target_column);
-        return -1;
-        break;
+        status = -1;
+        goto construct_validate_query_end;
       }
     } else
       for (int i = 0; i < schema_count; i++) {
         // find which entry in the schema matches
 
-        if (str_cci_cmp(target_column, schema[i].name) == 0) {
+        if (str_cci_cmp(column_name, schema[i].name) == 0) {
           // check if the input is a comment and the column does not have
           // comments.
           if (is_comments_column) {
@@ -135,72 +131,85 @@ int construct_validate_query(json_t *entry, struct data_column *schema,
 
     // also remember to catch when the key is not inside the table's schema
     if (!valid) {
-      free(target_column);
-      free(value_string);
-      return 0;
-    } else {
-      // @todo optimize
-      values_len += strlen(value_string) + 1;
-      column_names_len +=
-          strlen(key) + 1; // no need to use to_snake_case: it won't
-                           // change the length of the string.
+      status = 0;
+      goto construct_validate_query_end;
     }
-    free(target_column);
+    // @todo optimize
+    // add one because comma separation
+    column_names_size += strlen(key) + 1;
+    values_size +=
+        strlen(value_string) + 1; // no need to use to_snake_case: it won't
+                                  // change the length of the string.
+    free(column_name);
     free(value_string);
   }
-  values_len--;
-  column_names_len--;
+  column_name = NULL;
+  value_string = NULL;
 
-  // get ready and put in all the correct values
-  char *column_names = malloc(column_names_len + 1);
-  char *values = malloc(values_len + 1);
+  // populate column_names & values
 
-  // make them empty strings
-  strncpy(column_names, "", 1);
-  strncpy(values, "", 1);
+  column_names = malloc(column_names_size);
+  values = malloc(values_size);
 
+  // catch malloc failure
+  if (!column_names || !values) {
+    status = -1;
+    goto construct_validate_query_end;
+  }
+
+  char *column_names_cur = column_names;
+  char *values_cur = values;
+
+  // build the query
   json_object_foreach(entry, key, value) {
+    // skip ID column (rely on auto-increment)
     if (strcmp(key, "id") == 0)
       continue;
 
     value_string = json_to_string(value);
-    snake_case_key = malloc(strlen(key) + 1);
 
-    if (!value_string || !snake_case_key) {
+    // catch malloc failure
+    if (!value_string) {
       status = -1;
       goto construct_validate_query_end;
-
-      return -1;
     }
-    strncpy(snake_case_key, key, strlen(key) + 1);
-    to_snake_case(snake_case_key);
+    // deal with snake case later
+    memcpy(column_names_cur, key, strlen(key));
+    column_names_cur += strlen(key);
+    // add in the comma while moving the cursor forward
+    *column_names_cur++ = ',';
 
-    strncat(column_names, snake_case_key, strlen(key) + 1);
-    strncat(column_names, ",", 1 + 1);
-    strncat(values, value_string, strlen(value_string) + 1);
-    strncat(values, ",", 1 + 1);
+    memcpy(values_cur, value_string, strlen(value_string));
+    values_cur += strlen(value_string);
+    // add in the comma while moving the cursor forward
+    *values_cur++ = ',';
 
-    free(snake_case_key);
     free(value_string);
   }
+  value_string = NULL;
 
   // remove trailing commas
-  column_names[strlen(column_names) - 1] = '\0';
-  values[strlen(values) - 1] = '\0';
+  *--column_names_cur = '\0';
+  *--values_cur = '\0';
 
-  size_t query_len = strlen("INSERT INTO  () VALUES() RETURNING ;") +
-                     strlen(table_name) + (column_names_len) + (values_len) +
-                     strlen(primary_column_name) + 1;
-  *query = malloc(query_len);
-  if (!*query)
-    return -1;
-  snprintf(*query, query_len, "INSERT INTO %s (%s) VALUES(%s) RETURNING %s;",
+  // abuse the fact that to_snake_case completely ignores commas
+  to_lower_snake_case(column_names);
+
+  size_t query_size = strlen("INSERT INTO  () VALUES() RETURNING ;") +
+                      strlen(table_name) + (column_names_size - 1) +
+                      (values_size - 1) + strlen(primary_column_name) + 1;
+  *query = malloc(query_size);
+  if (!*query) {
+    status = -1;
+    goto construct_validate_query_end;
+  }
+  snprintf(*query, query_size, "INSERT INTO %s (%s) VALUES(%s) RETURNING %s;",
            table_name, column_names, values, primary_column_name);
-  return status;
 
 construct_validate_query_end:
+  free(column_name);
   free(value_string);
-  free(snake_case_key);
-
+  free(column_names);
+  free(values);
   return status;
 }
