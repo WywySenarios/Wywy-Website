5029cf4b67645e3c61fe899a37d7d0fd7bf3a1f1
WywySenarios <wywyericzhu@gmail.com>
2026-02-20 15:57:39 -0500

    Add NumberBox input element

---
+++ apps/astro-app/src/components/data/input-element/number-box.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState, type ChangeEvent } from "react";
+import { Input } from "@/components/ui/input";
+
+type InputProps = {
+  value: number;
+  onChange: (val: number) => void;
+  [key: string]: any;
+};
+
+export function NumberBox({ value, onChange, ...props }: InputProps) {
+  const [valid, setValid] = useState<boolean>(true);
+  return (
+    <Input
+      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+        let output: number = parseInt(e.target.value);
+        if (isNaN(output)) {
+          setValid(false);
+          return;
+        }
+
+        onChange(output);
+        setValid(true);
+      }}
+      aria-invalid={valid}
+      {...props}
+    />
+  );
+}
