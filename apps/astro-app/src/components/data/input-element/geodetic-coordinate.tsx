65cbda8c0ddf43ae70dabfd3277e669151b8a180
WywySenarios <wywyericzhu@gmail.com>
2026-02-20 16:20:18 -0500

    Add Cards and descriptions

--- apps/astro-app/src/components/data/input-element/geodetic-coordinate.tsx
+++ apps/astro-app/src/components/data/input-element/geodetic-coordinate.tsx
@@ -4,6 +4,7 @@ import { NumberBox } from "./number-box";
 import { Button } from "@/components/ui/button";
 import { LocateFixed } from "lucide-react";
 import { toast } from "sonner";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 // everything defaults to true
 export interface GeodeticCoordinateFields {
@@ -35,85 +36,128 @@ export function GeodeticCoordinateInputElement({
   return (
     <div>
       {/* latitude & longitude */}
-      <Slider
-        value={[value.latitude]}
-        onValueCommit={(latitude: number[]) => {
-          let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-          newValue.latitude = latitude[0];
-          onChange(newValue);
-        }}
-      />
-      <Slider
-        value={[value.longitude]}
-        onValueCommit={(longitude: number[]) => {
-          let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-          newValue.longitude = longitude[0];
-          onChange(newValue);
-        }}
-      />
-      {
-        /* Latitude & longitude accuracy */
-        value.accuracy != null ? (
-          <NumberBox
-            value={value.accuracy}
-            onChange={(accuracy: number) => {
+      <Card>
+        <CardHeader>Latitude</CardHeader>
+        <CardContent>
+          <Slider
+            value={[value.latitude]}
+            onValueCommit={(latitude: number[]) => {
               let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-              newValue.accuracy = accuracy;
+              newValue.latitude = latitude[0];
               onChange(newValue);
             }}
           />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>Longitude</CardHeader>
+        <CardContent>
+          <Slider
+            value={[value.longitude]}
+            onValueCommit={(longitude: number[]) => {
+              let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
+              newValue.longitude = longitude[0];
+              onChange(newValue);
+            }}
+          />
+        </CardContent>
+      </Card>
+      {
+        /* Latitude & longitude accuracy */
+        value.accuracy != null ? (
+          <Card>
+            <CardHeader>Latitude and longitude accuracy</CardHeader>
+            <CardContent>
+              <NumberBox
+                value={value.accuracy}
+                onChange={(accuracy: number) => {
+                  let newValue: GeodeticCoordinate = new GeodeticCoordinate(
+                    value,
+                  );
+                  newValue.accuracy = accuracy;
+                  onChange(newValue);
+                }}
+              />
+            </CardContent>
+          </Card>
         ) : null
       }
       {
         /* Altitude */
         value.altitude != null ? (
-          <Slider
-            value={[value.altitude]}
-            onValueCommit={(altitude: number[]) => {
-              let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-              newValue.altitude = altitude[0];
-              onChange(newValue);
-            }}
-          />
+          <Card>
+            <CardHeader>Altitude</CardHeader>
+            <CardContent>
+              <Slider
+                value={[value.altitude]}
+                onValueCommit={(altitude: number[]) => {
+                  let newValue: GeodeticCoordinate = new GeodeticCoordinate(
+                    value,
+                  );
+                  newValue.altitude = altitude[0];
+                  onChange(newValue);
+                }}
+              />
+            </CardContent>
+          </Card>
         ) : null
       }
       {
         /* Altitude accuracy */
         value.altitudeAccuracy != null ? (
-          <NumberBox
-            value={value.altitudeAccuracy}
-            onChange={(altitudeAccuracy: number) => {
-              let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-              newValue.altitudeAccuracy = altitudeAccuracy;
-              onChange(newValue);
-            }}
-          />
+          <Card>
+            <CardHeader>Altitude Accuracy</CardHeader>
+            <NumberBox
+              value={value.altitudeAccuracy}
+              onChange={(altitudeAccuracy: number) => {
+                let newValue: GeodeticCoordinate = new GeodeticCoordinate(
+                  value,
+                );
+                newValue.altitudeAccuracy = altitudeAccuracy;
+                onChange(newValue);
+              }}
+            />
+          </Card>
         ) : null
       }
       {
         /* Heading */
         value.heading != null ? (
-          <Slider
-            value={[value.heading]}
-            onValueCommit={(heading: number[]) => {
-              let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-              newValue.heading = heading[0];
-              onChange(newValue);
-            }}
-          />
+          <Card>
+            <CardHeader>Heading</CardHeader>
+            <CardContent>
+              <Slider
+                value={[value.heading]}
+                onValueCommit={(heading: number[]) => {
+                  let newValue: GeodeticCoordinate = new GeodeticCoordinate(
+                    value,
+                  );
+                  newValue.heading = heading[0];
+                  onChange(newValue);
+                }}
+              />
+            </CardContent>
+          </Card>
         ) : null
       }
       {
         /* Speed */
         value.speed != null ? (
-          <NumberBox
-            value={value.speed}
-            onChange={(speed: number) => {
-              let newValue: GeodeticCoordinate = new GeodeticCoordinate(value);
-              newValue.speed = speed;
-              onChange(newValue);
-            }}
-          />
+          <Card>
+            <CardHeader>Speed</CardHeader>
+            <CardContent>
+              <NumberBox
+                value={value.speed}
+                onChange={(speed: number) => {
+                  let newValue: GeodeticCoordinate = new GeodeticCoordinate(
+                    value,
+                  );
+                  newValue.speed = speed;
+                  onChange(newValue);
+                }}
+              />
+            </CardContent>
+          </Card>
         ) : null
       }
       {/* Set to current location button */}
