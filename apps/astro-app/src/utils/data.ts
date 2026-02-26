35d97ed71608cea0ba8bfc1c7f8b503ca6ded503
WywySenarios <wywyericzhu@gmail.com>
2026-02-25 22:12:54 -0500

    (Partially) Refractor serialization

--- apps/astro-app/src/utils/data.ts
+++ apps/astro-app/src/utils/data.ts
@@ -1,5 +1,10 @@
 import { z, ZodNumber, type ZodTypeAny } from "zod";
 import type { DataColumn, Datatype, TableInfo } from "@/types/data";
+import {
+  GeodeticCoordinate,
+  GetCurrentGeodeticCoordinatePromise,
+} from "./datatypes/geodetic";
+import type { JSONValue } from "./http";
 
 /**
  * Produces a ZodType with restrictions as defined by the given column schema.
@@ -91,6 +96,230 @@ export function getFallbackValue(
   }
 }
 
+type formatDatabaseValueParams =
+  | { datatype: "int" | "integer" | "float" | "number"; value: number }
+  | { datatype: "string" | "str" | "text"; value: string }
+  | { datatype: "bool" | "boolean"; value: boolean }
+  | { datatype: "date" | "time" | "timestamp"; value: Date }
+  | { datatype: "enum"; value: string }
+  | { datatype: "geodetic point"; value: GeodeticCoordinate };
+/**
+ * Formats a value so that it can be exported via JSON.
+ * @param input A record with only the datatype and the value to format.
+ */
+export function formatDatabaseValue(
+  input: formatDatabaseValueParams,
+): JSONValue {
+  switch (input.datatype) {
+    case "date":
+    case "time":
+    case "timestamp":
+      return `'${input.value.toISOString().slice(0, -1)}'`;
+    case "geodetic point":
+      return input.value.toString();
+    // most datatypes do not need conversion
+    default:
+      return input.value;
+  }
+}
+
+/**
+ * Attempts to parse a database value, expecting the given datatype. Throws an error if unsuccessful.
+ * @TODO integer verification.
+ * @param value The value that needs to be parsed.
+ * @param datatype @type {Datatype} The expected datatype of the value.
+ * @returns @type {string | number | boolean | Date} The date returned is UTC.
+ */
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: "int" | "integer" | "float" | "number",
+): number;
+function parseDatabaseValue(
+  value: string,
+  datatype: "string" | "str" | "text",
+): string;
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: "bool" | "boolean",
+): boolean;
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: "date" | "time" | "timestamp",
+): Date;
+function parseDatabaseValue(value: JSONValue, datatype: "enum"): string;
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: "geodetic point",
+): string;
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: Datatype,
+): string | number | boolean | Date;
+function parseDatabaseValue(
+  value: JSONValue,
+  datatype: Datatype,
+): string | number | boolean | Date {
+  switch (datatype) {
+    case "string":
+    case "str":
+    case "text":
+      if (typeof value != "string")
+        throw `Datatype mistmatch. Expected string but received ${typeof value}.`;
+      return value;
+    case "int":
+    case "integer":
+    case "float":
+    case "number":
+      switch (typeof value) {
+        case "number":
+        case "bigint":
+          return value;
+        case "string":
+          const result = parseInt(value);
+          if (isNaN(result))
+            throw `Failed to parse database value "${value}". Expected a number or number-like string.`;
+          return result;
+        default:
+          throw `Datatype mismatch. Expected number or number-like string but received ${typeof value}.`;
+      }
+    case "bool":
+    case "boolean":
+      switch (typeof value) {
+        case "boolean":
+          return value;
+        case "string":
+          switch (value.toLowerCase()) {
+            case "true":
+              return true;
+            case "false":
+              return false;
+            default:
+              throw `Failed to parse database value "${value}". Expected boolean or boolean-like string.`;
+          }
+        default:
+          throw `Datatype mismatch. Expected boolean or boolean-like string but received ${typeof value}.`;
+      }
+    // The database always stores UTC values.
+    case "date":
+    case "time":
+    case "timestamp":
+      switch (typeof value) {
+        case "string":
+          const result = new Date(value);
+          if (isNaN(result.getDate()))
+            throw `Failed to parse database value "${value}". Expected a ${datatype}-like string.`;
+          return result;
+        default:
+          throw `Datatype mismatch. Expected a ${datatype}-like string but received ${typeof value}.`;
+      }
+    case "enum":
+      return ""; // @TODO
+    case "geodetic point":
+      return ""; // @TODO
+  }
+}
+
+export interface formOutput {
+  data: Record<string, any>;
+  tags?: String[];
+  descriptors?: Record<string, any>;
+}
+
+/**
+ * Serializes data bound for a database.
+ * @param values The values that need to be serialized.
+ * @param schema The schema that relates to the given values.
+ * @returns An object filled with the formatted values, or a string with a corresponding error message if the schema does not match the values.
+ */
+function serializeDataObject(
+  values: Record<string, any>,
+  schema: Array<DataColumn>,
+): Record<string, any> {
+  let formattedValues: Record<string, any> = {};
+
+  for (let column_schema of schema) {
+    // check the comments first
+    if (column_schema.comments) {
+      formattedValues[`${column_schema.name}_comments`] =
+        values[`${column_schema.name}_comments`] ?? "''";
+    }
+
+    // check if the user really wanted to submit that or not
+    if (values[column_schema.name] == null) {
+      continue;
+    }
+
+    // single-quote strings, dates, times, and timestamps
+    switch (column_schema.datatype) {
+      case "string":
+      case "str":
+      case "text":
+        formattedValues[column_schema.name] = `'${values[column_schema.name]}'`;
+        break;
+      case "date":
+      case "time":
+      case "timestamp":
+        formattedValues[column_schema.name] =
+          `'${typeof values[column_schema.name] == "string" ? values[column_schema.name] : values[column_schema.name].toISOString()}'`.replace(
+            "Z",
+            "",
+          );
+        break;
+      default:
+        formattedValues[column_schema.name] = values[column_schema.name];
+        break;
+    }
+  }
+
+  return formattedValues;
+}
+
+/**
+ * Attempts to serialize database-bound data.
+ * @param data The data to serialize.
+ * @param strict Whether or not to throw out when a schema is incomplete (partially cooperative)
+ * @param tableInfo The table schema of the data.
+ */
+export function serializeFormOutput(
+  data: formOutput,
+  strict: boolean,
+  tableInfo: TableInfo,
+): formOutput {
+  let serializedValues: formOutput = {
+    data: serializeDataObject(data.data, tableInfo.schema),
+  };
+
+  if ("tags" in data && data["tags"]) {
+    if ("primary_tag" in data.data) {
+      throw "Primary tag already set? Contact dev for a fix.";
+    }
+
+    data.data["primary_tag"] = data.tags.shift();
+
+    if (strict && !data.data["primary_tag"]) {
+      throw "Primary tag cannot be empty.";
+    }
+
+    serializedValues["tags"] = data.tags;
+  }
+
+  if ("descriptors" in data && data.descriptors != undefined) {
+    serializedValues["descriptors"] = {};
+
+    // format every descriptor type.
+    for (const descriptor_schema of tableInfo.descriptors)
+      serializedValues.descriptors[descriptor_schema.name] = data.descriptors[
+        descriptor_schema.name
+      ].map((value: { [x: string]: any }) =>
+        serializeDataObject(value, descriptor_schema.schema),
+      );
+  }
+
+  return serializedValues;
+}
+
+// export function deserializedDatabaseRecord(data: Record<string, unknown>) {}
+
 /**
  * Checks if tagging is enabled on the given table.
  * @param tableInfo The schema of the table to check.
@@ -98,3 +327,5 @@ export function getFallbackValue(
 export function isTaggingEnabled(tableInfo: TableInfo): boolean {
   return tableInfo.tagging;
 }
+
+export { parseDatabaseValue };
