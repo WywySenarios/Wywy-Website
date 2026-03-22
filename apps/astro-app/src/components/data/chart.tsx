915fcdecd69cf0256c9ed16c63476cdd52658868
WywySenarios <wywyericzhu@gmail.com>
2026-03-22 12:43:06 -0400

    Move generic EChart to @components/data/chart.tsx

--- apps/astro-app/src/components/data/chart.tsx
+++ apps/astro-app/src/components/data/chart.tsx
@@ -3,6 +3,15 @@ import { GaugeChart } from "./chart/gauge-chart";
 import { WaterfallChart } from "./chart/waterfall-chart";
 import { TreeMap } from "./chart/tree-map";
 import { coerceToNumber } from "@utils/math/datatype";
+import {
+  getInstanceByDom,
+  init,
+  type ECharts,
+  type EChartsOption,
+  type SetOptionOpts,
+} from "echarts";
+import { useEffect, useRef, type JSX } from "react";
+import { cn } from "@root/lib/utils";
 import { toSnakeCase } from "@utils/parse";
 
 export function GenericEmptyChart() {
@@ -47,4 +56,62 @@ export function GenericChart({ data, options }: GenericChartProps) {
     default:
       console.warn(`Could not find chart type "${chartType}"`);
   }
+}
+
+export function EChart({
+  id,
+  className,
+  options,
+  optionSettings,
+  showLoading,
+  theme,
+}: React.ComponentProps<"div"> & {
+  options: EChartsOption;
+  optionSettings?: SetOptionOpts;
+  showLoading?: boolean;
+  theme?: "light" | "dark";
+}): JSX.Element {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Initialize chart
+    let chart: ECharts | undefined;
+    if (chartRef.current !== null) {
+      chart = init(chartRef.current, theme);
+    }
+
+    function resizeChart() {
+      chart?.resize();
+    }
+    window.addEventListener("resize", resizeChart);
+
+    // Cleanup function
+    return () => {
+      chart?.dispose();
+      window.removeEventListener("resize", resizeChart);
+    };
+  }, [theme]);
+
+  useEffect(() => {
+    // Update chart
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      console.log(options);
+      chart?.setOption(options, optionSettings);
+    }
+  }, [options, optionSettings, theme]); // Whenever theme changes we need to add options and setting due to it being deleted in cleanup function
+
+  useEffect(() => {
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      showLoading === true ? chart?.showLoading() : chart?.hideLoading();
+    }
+  }, [showLoading, theme]);
+
+  return (
+    <div
+      ref={chartRef}
+      className={cn("min-w-100 min-h-100 w-full h-full", className)}
+    />
+  );
 }
