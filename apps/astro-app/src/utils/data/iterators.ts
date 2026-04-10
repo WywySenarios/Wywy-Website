67b4c3ffacfeeeb432ea8f7d3c596356d91dcb0a
WywySenarios <wywyericzhu@gmail.com>
2026-04-10 14:29:32 -0400

    DRAFT: iterators
    Create an iterator to resolve columns.
    
    This will be a major improvement because new Wywy-Website code may no longer miss comments columns.

---
+++ apps/astro-app/src/utils/data/iterators.ts
@@ -0,0 +1,97 @@
+import type { DataColumn, ResolvedColumnSchema } from "@/types/data";
+
+/**
+ * Resolves a column and its sub columns.
+ * @param column The column schema of the column to resolve.
+ */
+export function* resolveColumnSchema(
+  column: DataColumn,
+): Generator<ResolvedColumnSchema> {
+  switch (column.datatype) {
+    case "bool":
+    case "boolean":
+      yield {
+        ...column,
+        datatype: "bool",
+        entrytype: "none",
+      };
+      break;
+    case "date":
+    case "time":
+    case "timestamp":
+    case "enum":
+      yield {
+        ...column,
+        entrytype: "none",
+      };
+      break;
+    case "float":
+    case "number":
+      yield {
+        ...column,
+        datatype: "number",
+        entrytype: "none",
+      };
+      break;
+    case "geodetic point":
+      yield {
+        ...column,
+        entrytype: "none",
+      };
+      yield {
+        ...column,
+        name: column.name + "_latlong_accuracy",
+        datatype: "number",
+        entrytype: "none",
+        defaultValue: undefined,
+      };
+      yield {
+        ...column,
+        name: column.name + "_altitude",
+        datatype: "number",
+        entrytype: "none",
+        defaultValue: undefined,
+      };
+      yield {
+        ...column,
+        name: column.name + "_altitude_accuracy",
+        datatype: "number",
+        entrytype: "none",
+        defaultValue: undefined,
+      };
+      break;
+    case "int":
+    case "integer":
+      yield {
+        ...column,
+        datatype: "int",
+        entrytype: "none",
+      };
+      break;
+    case "str":
+    case "string":
+    case "text":
+      yield { ...column, datatype: "str", entrytype: "none" };
+      break;
+  }
+
+  if (column.comments) {
+    yield {
+      name: column.name + "_comments",
+      entrytype: "none",
+      datatype: "str",
+    };
+  }
+}
+
+/**
+ * Resolves a columns schema.
+ * @param columnsSchema
+ */
+export function* resolveColumnsSchema(
+  columnsSchema: DataColumn[],
+): Generator<ResolvedColumnSchema> {
+  for (const columnSchema of columnsSchema) {
+    yield* resolveColumnSchema(columnSchema);
+  }
+}
